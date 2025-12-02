761fe1afe9baaa2e87dd3803144078d1276a1a44
joaovitubr <joaovmachado10@gmail.com>
2025-12-02 11:21:16 -0300

    chore: simplified PR generation and disabled diff report generation

--- .github/scripts/open-pr.js
+++ .github/scripts/open-pr.js
@@ -1,12 +1,10 @@
-const fs = require("fs");
-
 module.exports = async ({ github, context, core }) => {
   const { owner, repo } = context.repo;
 
   const version = process.env.NEWEST_VERSION;
   const branchName = `preview/v${version}`;
-  const title = `v${version}`;
-  const body = fs.readFileSync("out/pr-description.md", "utf8");
+  const title = `New version ${version}`;
+  const body = "";
 
   const pr = await github.rest.pulls.create({
     owner,
